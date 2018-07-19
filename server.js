b4ed9f1f46df253164ea694ea22f332117871b7e
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-07-18 23:56:10 -0500

    another try getting closer

--- server.js
+++ server.js
@@ -15,10 +15,10 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
-const path = require('path');
+// const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, '/index.html');
+// const INDEX = path.join(__dirname, '/index.html');
 
 // const server = express()
 //     .use((req, res) => res.sendFile(INDEX))
