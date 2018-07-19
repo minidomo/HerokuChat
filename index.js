c8fe7dd9e438a87bdb0ba423ee8df0d0c87a2a63
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-07-19 00:29:03 -0500

    antoher try

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+// var express = require('express');
+// var app = express();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
 
-server.listen(80);
+// server.listen(80);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-io.on('connection', function (socket) {
-    socket.on('chat-message', function (data) {
-        io.sockets.emit('chat-message', data);
-    });
+// io.on('connection', function (socket) {
+//     socket.on('chat-message', function (data) {
+//         io.sockets.emit('chat-message', data);
+//     });
+// });
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+// const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+// const INDEX = path.join(__dirname, 'index.html');
+
+// const server = express()
+//     .use((req, res) => res.sendFile(INDEX))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const server = express()
+    .use(express.static('public'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));;
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
