655d14abc4212ac3cdecc8951112c76401e95ffe
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-07-19 00:19:06 -0500

    another try

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// var express = require('express');
-// var app = express();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-
-// server.listen(80);
-
-// app.use(express.static('public'));
-
-// io.on('connection', function (socket) {
-//     socket.on('chat-message', function (data) {
-//         io.sockets.emit('chat-message', data);
-//     });
-// });
-
 'use strict';
 
 const express = require('express');
@@ -26,10 +11,6 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// const server = express()
-//     .use(express.static('public'))
-//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
@@ -37,5 +18,3 @@ io.on('connection', (socket) => {
         io.sockets.emit('chat-message', data);
     });
 });
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
