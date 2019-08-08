73a5bc177417f1b337c27905a9c7061f429baac6
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-08-08 12:07:16 -0500

    fixed port

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express().use(express.static('public'));
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => console.log('Listening to port:3000'));
+server.listen(PORT, () => console.log(`Listening to port:${PORT}`));
 
 io.on('connection', socket => {
     console.log(socket.id + ' connected');
