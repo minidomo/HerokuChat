a06265583dd49d3fd8959b2d6696cc22056b16f1
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2020-01-09 23:12:05 -0600

    moved file

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
-console.log(__dirname + '\\..\\public');
 const express = require('express');
-const app = express().use(express.static(__dirname + '\\..\\public'));
+const app = express().use(express.static('public'));
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening to port:${PORT}`));
 
-const eventHandler = require('./handler/eventHandler');
+const eventHandler = require('./src/handler/eventHandler');
 eventHandler.start(io);
